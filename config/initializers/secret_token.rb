7ca2c73d7bd994071994f4da0b481a610848a943
Albert Tseng <albert@chegg.com>
2015-09-28 12:07:16 -0700

    initial ci, setting up product page with scaffold and bootstrap for styling

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackatools::Application.config.secret_key_base = 'e2952e88c7796c233c74c2b92d255eaa654a26919337ca54b0ac0403818e8d0c778f8443fb395338a0c742cd7725a4b202163faa5b4c25b0abe3aff17fd468ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hackatools::Application.config.session_store :cookie_store, key: '_hackatools_session'
