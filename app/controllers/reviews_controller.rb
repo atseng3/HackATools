cd93e68b8c8e2eeaa68ed6586b5b3b283081552a
Albert Tseng <albert@chegg.com>
2015-09-28 15:21:13 -0700

    Link Reviews to Users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,8 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_review, only: [:edit, :update, :destroy]
+  # before_action :set_restaurant
+  before_action :authenticate_user!
+  # before_action :check_user, only: [:edit, :update, :destroy]
 
   # GET /reviews
   # GET /reviews.json
@@ -25,6 +28,7 @@ class ReviewsController < ApplicationController
   # POST /reviews.json
   def create
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
 
     respond_to do |format|
       if @review.save
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928215118) do
+ActiveRecord::Schema.define(version: 20150928221829) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150928215118) do
     t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
