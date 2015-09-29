80dbf88b5621a627f1abcff5540c91bcf50d61fe
Albert Tseng <albert@chegg.com>
2015-09-28 18:21:48 -0700

    add data validations and errors

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	has_many :reviews
+	validates :name, :website, presence: true
+	validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+	  message: "must start with http:// or https://" }
 end
